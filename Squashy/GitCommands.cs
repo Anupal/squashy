a9cd621a287404cbc58ac3f39f3d0196add85d1f
Anupal Mishra <anupal.pm@gmail.com>
2025-07-06 00:14:26 +0100

    tabular output for git log

--- Squashy/GitCommands.cs
+++ Squashy/GitCommands.cs
@@ -6,10 +6,27 @@ public class GitCommands
     {
         using var repo = new Repository(directory);
         var filter = new CommitFilter { SortBy = CommitSortStrategies.Topological | CommitSortStrategies.Reverse };
-
-        foreach (Commit commit in repo.Commits.QueryBy(filter).Take(numCommits))
+        printCommitsTable(repo.Commits.QueryBy(filter).Take(numCommits), true);
+    }
+    
+    private void printCommitsTable(IEnumerable<Commit> commitList, bool displayHeader = false)
+    {
+        if (displayHeader)
         {
-            Console.WriteLine($"commit {commit.Id} - {commit.Message[..^1]}");
+            Console.WriteLine("{0,-7} {1,-20} {2,-50}", "Hash", "Author", "Message");
+            Console.WriteLine(new string('-', 40));
+        }
+        
+        foreach (Commit commit in commitList)
+        {
+            string shortHash = commit.Sha.Substring(0, 7);
+            string author = commit.Author.Name;
+            string message = commit.MessageShort;
+
+            // Manually truncate to match desired max lengths
+            if (author.Length > 20) author = author.Substring(0, 20);
+            if (message.Length > 50) message = message.Substring(0, 50);
+            Console.WriteLine("{0,-7} {1,-20} {2,-50}", shortHash, author, message);
         }
     }
 }
