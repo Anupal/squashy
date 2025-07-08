9c5955b4c0559fa60aa70a03b53f90659046ef43
Anupal Mishra <anupal.pm@gmail.com>
2025-07-08 23:33:17 +0100

    Add scaffolding for squash

--- Squashy/GitCommands.cs
+++ Squashy/GitCommands.cs
@@ -4,7 +4,7 @@ public class GitCommands
 {
     private string directory;
     private Repository repo;
-    
+
 
     public GitCommands(string directory)
     {
@@ -12,19 +12,34 @@ public class GitCommands
         this.directory = directory;
         this.repo = new Repository(directory);
     }
-    
+
     public void Log(int numCommits)
     {
         printCommitsTable(getCommits(numCommits), true);
     }
 
-    public IEnumerable<Commit> getCommits()
+    public void Squash(string firstCommitSha, string secondCommitSha, bool dryRun)
+    {
+        using var repo = new Repository(directory);
+        var firstCommit = fetchCommitBySha(repo, firstCommitSha);
+        var secondCommit = fetchCommitBySha(repo, secondCommitSha);
+
+        if (firstCommit == null || secondCommit == null) { return; }
+
+
+        if (dryRun)
+        {
+            squashDryRun(getCommits(), firstCommitSha, secondCommitSha);
+        }
+    }
+
+    private IEnumerable<Commit> getCommits()
     {
         var filter = new CommitFilter { SortBy = CommitSortStrategies.Topological | CommitSortStrategies.Reverse };
         return repo.Commits.QueryBy(filter);
     }
 
-    public IEnumerable<Commit> getCommits(int numCommits)
+    private IEnumerable<Commit> getCommits(int numCommits)
     {
         var filter = new CommitFilter { SortBy = CommitSortStrategies.Topological | CommitSortStrategies.Reverse };
         return repo.Commits.QueryBy(filter).Take(numCommits);
@@ -49,5 +64,41 @@ public class GitCommands
             if (message.Length > 50) message = message.Substring(0, 50);
             Console.WriteLine("{0,-7} {1,-20} {2,-50}", shortHash, author, message);
         }
+    }
+    
+    /// <summary>
+    ///  Checks if the commit exists for the given SHA and is present in the current branch.
+    /// </summary>
+    /// <returns>The retrieved <c>Commit</c> object or <c>null</c> if not found or not present in the current branch.</returns>
+    private Commit fetchCommitBySha(Repository repo, string commitSha)
+    {
+        if (commitSha.Length != 7 && commitSha.Length != 40)
+        {
+            Console.WriteLine($"Invalid commit SHA length for '{commitSha}'. Expected 7 or 40 characters, got {commitSha.Length}");
+            return null;
+        }
+
+        var commit = repo.Lookup<Commit>(commitSha);
+        if (commit == null)
+        {
+            Console.WriteLine($"Commit '{commitSha}' does not exist!");
+            return null;
+        }
+
+        // check if commit is in the current branch
+        var currentBranch = repo.Head;
+        var existsInBranch = repo.Commits.QueryBy(new CommitFilter { IncludeReachableFrom = currentBranch }).Any(c => c.Sha.StartsWith(commitSha));
+        if (!existsInBranch)
+        {
+            Console.WriteLine($"Commit '{commitSha}' does not exist in the current branch");
+            return null;
+        }
+
+        return commit;
+    }
+
+    private void squashDryRun(IEnumerable<Commit> commits, string firstCommitSha, string secondCommitSha)
+    {
+
     }
 }
--- Squashy/Program.cs
+++ Squashy/Program.cs
@@ -11,8 +11,8 @@ class Program
             Description = "Git directory",
             DefaultValueFactory = parseResult => ".",
         };
-        Command logCommitsCmd = new("log", "List last N commits");
 
+        Command logCommitsCmd = new("log", "List last N commits");
         Option<int> numCommitOptions = new("-n")
         {
             Description = "Number of commits",
@@ -26,8 +26,39 @@ class Program
             return 0;
         });
 
+        Command squashCommitsCmd = new(
+            "squash",
+            "Squash all commits between the specified start and end commits (inclusive)."
+        );
+        var firstCommitArg = new Argument<string>("firstCommit")
+        {
+            Description = "The SHA of the first (older) commit."
+        };
+        var secondCommitArg = new Argument<string>("secondCommit")
+        {
+            Description = "The SHA of the second (newer) commit."
+        };
+        Option<bool> dryRunOption = new("--dry-run", "-x")
+        {
+            Description = "To show expected output.",
+            DefaultValueFactory = parseResult => false,
+        };
+        squashCommitsCmd.Add(firstCommitArg);
+        squashCommitsCmd.Add(secondCommitArg);
+        squashCommitsCmd.Add(dryRunOption);
+        squashCommitsCmd.SetAction(parseResult =>
+        {
+            GitCommands git = new(parseResult.GetValue(directoryOption));
+            git.Squash(
+                parseResult.GetValue(firstCommitArg),
+                parseResult.GetValue(secondCommitArg),
+                parseResult.GetValue(dryRunOption));
+            return 0;
+        });
+
         rootCmd.Add(directoryOption);
         rootCmd.Add(logCommitsCmd);
+        rootCmd.Add(squashCommitsCmd);
 
         ParseResult parseResult = rootCmd.Parse(args);
         return parseResult.Invoke();
