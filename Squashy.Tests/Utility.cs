e647a4a4f5dd52ff5f20bb382bf046df3ecb7640
Anupal Mishra <anupal.pm@gmail.com>
2025-07-13 23:40:10 +0100

    Implement Squash logic and NUnit tests (#6)
    * core squash logic
    * add tests for squash and log

---
+++ Squashy.Tests/Utility.cs
@@ -0,0 +1,68 @@
+using LibGit2Sharp; 
+using NUnit.Framework;
+using System.IO;
+
+namespace Squashy.Tests;
+
+static class Utility
+{
+    public static void CreateTestRepo(string testRepoPath) {
+        Console.WriteLine($"Setting up test repo '{testRepoPath}'"); 
+        // Delete repo if it already exists
+        if (Directory.Exists(testRepoPath))
+        {
+            Directory.Delete(testRepoPath, recursive: true);
+        }
+        Directory.CreateDirectory(testRepoPath);
+        Repository.Init(testRepoPath);
+    }
+
+    public static void CreateFileCommits(string testRepoPath, int numOfFiles = 3) {
+        Repository repo = new Repository(testRepoPath);
+        repo.Refs.UpdateTarget("HEAD", "refs/heads/main");
+
+        // Add files
+        for (int i = 1; i <= numOfFiles; i++) {
+            string filePath = Path.Combine(testRepoPath, $"file{i}.txt");
+            File.WriteAllText(filePath, $"file{i} content");
+
+            var sig = new Signature($"Test user {i}", $"testuser{i}@test.com", DateTimeOffset.Now);
+            Commands.Stage(repo, filePath);
+            repo.Commit($"create file {i}", sig, sig);
+        }
+
+        // Modify files
+        for (int i = 1; i <= numOfFiles; i++) {
+            string filePath = Path.Combine(testRepoPath, $"file{i}.txt");
+            File.AppendAllText(filePath, " modified..");
+            
+            var sig = new Signature($"Test user {i}", $"testuser{i}@test.com", DateTimeOffset.Now);
+            Commands.Stage(repo, filePath);
+            repo.Commit($"modify file {i}", sig, sig);
+        }
+
+        // Delete files
+        for (int i = 1; i <= numOfFiles; i++) {
+            string filePath = Path.Combine(testRepoPath, $"file{i}.txt");
+            File.Delete(filePath);
+            
+            var sig = new Signature($"Test user {i}", $"testuser{i}@test.com", DateTimeOffset.Now);
+            Commands.Stage(repo, filePath);
+            repo.Commit($"delete file {i}", sig, sig);
+        }
+    }
+
+    public static List<string> GetAllCommitHashes(string testRepoPath)
+    {
+        Repository repo = new Repository(testRepoPath);
+        var filter = new CommitFilter { SortBy = CommitSortStrategies.Topological };
+        return repo.Commits.QueryBy(filter).Select(c => c.Id.Sha.Substring(0, 7)).ToList();
+    }
+
+    public static List<string> GetAllCommitMessages(string testRepoPath)
+    {
+        Repository repo = new Repository(testRepoPath);
+        var filter = new CommitFilter { SortBy = CommitSortStrategies.Topological };
+        return repo.Commits.QueryBy(filter).Select(c => c.MessageShort).ToList();
+    }
+}
--- Squashy/GitCommands.cs
+++ Squashy/GitCommands.cs
@@ -36,6 +36,10 @@ public class GitCommands
         {
             squashDryRun(repo, firstCommit, secondCommit, message);
         }
+        else
+        {
+            squashExec(repo, firstCommit, secondCommit, message);
+        }
     }
 
     private IEnumerable<Commit> getCommits()
@@ -116,16 +120,7 @@ public class GitCommands
         // if secondCommit is not the head, we need to print commits that come after it
         if (!isHeadCommit(repo, secondCommit))
         {
-            var commitsAfterSecond = repo.Commits.QueryBy(
-                new CommitFilter
-                {
-                    IncludeReachableFrom = repo.Head,
-                    ExcludeReachableFrom = secondCommit,
-                    SortBy = CommitSortStrategies.Topological
-                }
-            ).SkipWhile(commit => commit.Sha == secondCommit.Sha);
-
-            printCommitsTable(commitsAfterSecond);
+            printCommitsTable(getDescendentsOfCommit(secondCommit));
         }
         Console.WriteLine($"<squashed commit> '{message}'");
 
@@ -138,6 +133,42 @@ public class GitCommands
             }
         ).SkipWhile(commit => commit.Sha == firstCommit.Sha);
         printCommitsTable(commitsBeforeFirst);
+    }
+
+    /// <summary>
+    /// Squash commits in the given range (inclusive).
+    /// </summary>
+    /// <param name="repo"></param>
+    /// <param name="firstCommit"></param>
+    /// <param name="secondCommit"></param>
+    /// <param name="message"></param>
+    private void squashExec(Repository repo, Commit firstCommit, Commit secondCommit, string message)
+    {
+        var userSignature = getAuthorFromGlobalConfig(repo);
+        var currentBranch = repo.Branches.FirstOrDefault(b => b.IsCurrentRepositoryHead);
+
+        // get all commits after secondCommit
+        var commitsAfterSecond = getDescendentsOfCommit(secondCommit);
+
+        // if secondCommit is not the head then discard all changes after it
+        if (!isHeadCommit(repo, secondCommit))
+        {
+            repo.Reset(ResetMode.Hard, secondCommit);
+        }
+
+        // check if firstCommit is the root commit of the branch
+        var previousCommit = firstCommit.Parents.FirstOrDefault();
+        if (previousCommit == null)
+        {
+            squashAllCommitsIncludingRoot(currentBranch, userSignature, message);
+        }
+        else
+        {
+            squashAllCommits(previousCommit, userSignature, message);
+        }
+
+        // cherry pick all commits after secondCommit
+        cherryPickListOfCommits(commitsAfterSecond);
     }
 
     /// <summary>
@@ -162,4 +193,85 @@ public class GitCommands
     {
         return repo.ObjectDatabase.FindMergeBase(firstCommit, secondCommit)?.Sha == firstCommit.Sha;
     }
+
+    /// <summary>
+    /// Get user details for commit author metadata.
+    /// </summary>
+    /// <param name="repo"></param>
+    /// <returns><c>Signature</c> object with user details.</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    private Signature getAuthorFromGlobalConfig(Repository repo)
+    {
+        // Load global or repo config
+        var config = repo.Config;
+
+        string name = config.Get<string>("user.name")?.Value;
+        string email = config.Get<string>("user.email")?.Value;
+
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email))
+        {
+            throw new InvalidOperationException("Global Git user.name or user.email is not configured.");
+        }
+
+        return new Signature(name, email, DateTimeOffset.Now);
+    }
+
+    /// <summary>
+    /// If entire commit history after a particular commit needs to be squashed.
+    /// This is do a soft rest and then commit all the changes.
+    /// </summary>
+    private void squashAllCommits(Commit commit, Signature userSignature, string message)
+    {
+        repo.Reset(ResetMode.Soft, commit);
+        repo.Commit(message, userSignature, userSignature);
+    }
+
+
+    /// <summary>
+    /// If entire commit history needs to be squashed including root.
+    /// This will stash all changes in the history and commit to an empty tree and then set HEAD to the new root commit.
+    /// </summary>
+    /// <param name="currentBranch"></param>
+    /// <param name="userSignature"></param>
+    /// <param name="message"></param>
+    private void squashAllCommitsIncludingRoot(Branch currentBranch, Signature userSignature, string message)
+    {
+        Commands.Stage(repo, "*");
+        var commit = repo.ObjectDatabase.CreateCommit(
+            userSignature,
+            userSignature,
+            message,
+            repo.ObjectDatabase.CreateTree(repo.Index),
+            parents: Enumerable.Empty<Commit>(),
+            prettifyMessage: false
+        );
+        repo.Refs.UpdateTarget(currentBranch.Reference, commit.Id);
+        repo.Refs.UpdateTarget("HEAD", currentBranch.CanonicalName);
+    }
+
+    private void cherryPickListOfCommits(IEnumerable<Commit> commitList)
+    {
+        foreach (Commit commit in commitList)
+        {
+            repo.CherryPick(commit, commit.Author);
+        }
+    }
+
+    /// <summary>
+    /// Get list of commits between HEAD and given commit
+    /// </summary>
+    /// <param name="commit"></param>
+    /// <returns>List of commits</returns>
+    private IEnumerable<Commit> getDescendentsOfCommit(Commit commit)
+    {
+        return repo.Commits.QueryBy(
+            new CommitFilter
+            {
+                IncludeReachableFrom = repo.Head,
+                ExcludeReachableFrom = commit,
+                SortBy = CommitSortStrategies.Topological
+            }
+        ).SkipWhile(c => c.Sha == commit.Sha);
+    } 
 }
+
